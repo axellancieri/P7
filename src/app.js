b04d7a91cc58ff6d52b2ce1b768091511f647be1
Azpers <axel.lavignasse@gmail.com>
2022-10-05 21:33:20 -0300

    wrapped up bell svg and js div with bell green dot notif animatitions using jq

--- src/app.js
+++ src/app.js
@@ -2,29 +2,39 @@
 const traffic = document.querySelector('.traffic');
 const getDiv = document.createElement('div');
 const main = document.querySelector('main');
+// const bellNotif = document.querySelector('.svg-head-bell ellipse');
+
 
 //Notification alerts
 function alertPopUp() {
     getDiv.innerHTML = `<p><strong>Alert:</strong> &nbsp You have unread messages</p><span>X</span>`;
     getDiv.setAttribute('id', 'notif-alert');
-    const closeNotifBar = getDiv.querySelector('span');
-    main.insertBefore(getDiv, traffic);
+    main.insertBefore(getDiv, traffic);   
     // making event listener on 'X' span element so it'll close the alertpopu
-    // closeNotifBar.style.cursor = 'pointer';
-    // closeNotifBar.addEventListener('click', () => {
-    //     $("#notif-alert").hide();
-    // });
-    //Using Jquery to give an animation sequence effect
+    const closeNotifBar = getDiv.querySelector('span');
+    closeNotifBar.style.cursor = 'pointer';
+    closeNotifBar.addEventListener('click', () => {
+        $("#notif-alert").hide();
+        $(".svg-head-bell ellipse").hide();
+    });
+    notifAlerts()
+}
+// Alerts function
+function notifAlerts() {
     $("#notif-alert")
         .hide()
-    //     .delay(500)
-    //     .slideDown(1500)
-    //     .delay(5000)
-    //     .slideUp(1500);
-};
-
+        .delay(500)
+        .slideDown(500)
+        .delay(4000)
+        .slideUp(500);
+    $(".svg-head-bell ellipse")
+        .hide()
+        .delay(500)
+        .show(500)
+        .delay(4000)
+        .hide(500);
+}
 //Making alertPop appear on page load
 window.addEventListener('load', () => {
     alertPopUp();
 });
-
