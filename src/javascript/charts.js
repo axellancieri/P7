7f5a45bfe29bad5aefc26a88f02294da14a37c2d
Azpers <axel.lavignasse@gmail.com>
2022-10-08 17:58:33 -0300

    chart looking good, resizing good too. Looking into breathing some life into it with js now for the buttons before moving down to the other charts

--- src/javascript/charts.js
+++ src/javascript/charts.js
@@ -16,8 +16,14 @@ const data = {
     labels: labels,
     datasets: [{
       label: 'Line Charts',
-      backgroundColor: 'rgb(255, 99, 132)',
-      borderColor: 'rgb(255, 99, 132)',
+      backgroundColor: 'rgba(76, 71, 175, 0.2)',
+      borderColor: 'rgba(76, 71, 175, 0.3)',
+      borderWidth: 1,
+      fill: true,
+      tension: 0.4,
+      pointStyle: 'circle',
+      pointRadius: 3,
+      pointHoverRadius: 1.5,
       data: [800, 1400, 1000, 2000, 1500, 1750, 1350, 1900, 2200, 1500, 2500],
     }]
   };
@@ -28,13 +34,23 @@ const config = {
     options: { maintainAspectRatio: false,
                scales: {
                     y: {
-                        beginAtZero: true
+                        beginAtZero: true,
+                        suggestedMin: 0,
+                        suggestedMax: 2500,
+                        alignToPixels: true,
+                        ticks: {
+                            stepSize: 500,
+                            font: {
+                                size: 9.2,
+                                weight: 'bold'
+                            }
+                        }
                     }
                 },
                 plugins: {
                     legend: {
                         display: false
-                    }
+                    },
                 }
     }
 };
