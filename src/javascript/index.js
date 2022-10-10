3bb261ee6c984c8fd6f15f040b7f819429a95853
Azpers <axel.lavignasse@gmail.com>
2022-10-10 02:06:07 -0300

    made social stats entirely appear through js trying some nasty refactoring and arrayed objects stuff

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,8 +1,10 @@
 import { alertPopUp } from './alertLoad'
+import { socialStats } from './alertLoad'
 import '../scss/style.scss'
 import './charts/charts'
 
 //Making alertPop appear on page load
 window.addEventListener('load', () => {
     alertPopUp();
+    socialStats();
 });
