7e6640c430f3185b2713c6345f520bc46c46a933
Azpers <Azpers@github.com>
2022-10-15 15:00:44 -0300

    320p seems good to go, working on 768 now

--- src/javascript/interactives.js
+++ src/javascript/interactives.js
@@ -92,13 +92,17 @@
     }
 
     function buttonMessage() {
+
         const button = messageForm.querySelector('button');
         const searchCheck = messageForm.querySelector('.message-search');
         const txtAreaCheck = messageForm.querySelector('.textarea');
+
         const alertMessageDiv = document.createElement('div');
         alertMessageDiv.classList.add('textarea-errormsg');
+
         const errorDetailsDiv = document.createElement('Div');
         errorDetailsDiv.classList.add('textarea-errormsg');
+
         button.insertAdjacentElement('beforebegin', alertMessageDiv);
 
         // making this event so it wont reload page
@@ -112,15 +116,28 @@
         button.addEventListener('mousedown', () => {
             for ( i = 0; i < pplData.length; i++ ) {
                 if (searchCheck.value.includes(pickUrValue(pplData, [i], 'name')) && !!txtAreaCheck.value && txtAreaCheck.value.length === txtAreaCheck.value.trim().length) {
+                    /* Following 2 lines make the animation proc if you change from success message to fail one
+                    Learned this at https://css-tricks.com/restart-css-animation/ */
+                    alertMessageDiv.classList.remove("textarea-errormsg-animation");
+                    void alertMessageDiv.offsetWidth;
+
                     alertMessageDiv.style.backgroundColor = 'rgba(76, 71, 175, 0.776)';
-                    alertMessageDiv.innerHTML = `<p>Message Sent! you'll receive a copy on your inbox</p>`
+                    alertMessageDiv.classList.add('textarea-errormsg-animation');
+                    alertMessageDiv.innerHTML = `<p>Message Sent! you'll receive a copy on your inbox</p>`;
                     break
                 } else {
+                    /* Following 2 lines make the animation proc if you change from success message to fail one
+                    Learned this at https://css-tricks.com/restart-css-animation/ */
+                    void alertMessageDiv.offsetWidth;
+                    alertMessageDiv.classList.remove("textarea-errormsg-animation");
+                    
                     alertMessageDiv.style.backgroundColor = 'rgba(225, 34, 34, 0.5607843137)';
+                    alertMessageDiv.classList.add('textarea-errormsg-animation');
                     alertMessageDiv.innerHTML = `<p>ERROR<br> <span id="errorMessage">Read more</span> to get details</p>`
                     /*Making an error message dropdown text with more details*/
                     const errorMessage = alertMessageDiv.querySelector('#errorMessage');
                     errorDetailsDiv.style.backgroundColor = 'rgba(225, 34, 34, 0.5607843137)';
+                    errorDetailsDiv.classList.add('textarea-errorreadme');
                     errorDetailsDiv.innerHTML = '<p>Check for empty spaces at the beginning of your text and that the name and last name of the person sending the message is correct. <span id="errorDetails">Close Read more</span></p>';
                     errorMessage.addEventListener('mousedown', () => {
                         alertMessageDiv.insertAdjacentElement('afterend', errorDetailsDiv)
@@ -135,64 +152,67 @@
     //message user button interactions
 
 //Settings local storage
-const settings = document.querySelector('.settings');
-const settButtons = settings.querySelectorAll('.btn-settings');
-const saveSett = settButtons[0];
-const cancelSett = settButtons[1];
-const emailNotif = settings.querySelector('#chkboxEmail');
-const setProf = settings.querySelector('#chkboxSetProf');
-const timeZone = settings.querySelector('.time-zone');
+    const settings = document.querySelector('.settings');
+    const settButtons = settings.querySelectorAll('.btn-settings');
+    const saveSett = settButtons[0];
+    const cancelSett = settButtons[1];
+    const emailNotif = settings.querySelector('#chkboxEmail');
+    const setProf = settings.querySelector('#chkboxSetProf');
+    const timeZone = settings.querySelector('.time-zone');
 
 /* function with checkboxes and timezone function */
 
-function localStorSettings() {
-    const localStorCheckBox = function(checkboxVar, objName) {
-        saveSett.addEventListener('click', () => {
-            if (checkboxVar.checked === true) {
-                localStorage.setItem(`${objName}`, 'true')
+    function localStorSettings() {
+        const localStorCheckBox = function(checkboxVar, objName) {
+            saveSett.addEventListener('click', () => {
+                if (checkboxVar.checked === true) {
+                    localStorage.setItem(`${objName}`, 'true')
+                } else {
+                    localStorage.setItem(`${objName}`, 'false')
+                }
+            })
+            
+            localStorage.getItem(`${objName}`);
+            if (localStorage.getItem(`${objName}`) === 'true') {
+                checkboxVar.checked = true;
             } else {
-                localStorage.setItem(`${objName}`, 'false')
+                checkboxVar.checked = false;
             }
-        })
+        }
         
-        localStorage.getItem(`${objName}`);
-        if (localStorage.getItem(`${objName}`) === 'true') {
-            checkboxVar.checked = true;
-        } else {
-            checkboxVar.checked = false;
+        const localStorTimeZone = function() {
+
+            /*Making the following if statement so it'll show 'select timezone'
+            when openning the site without any local storage */
+
+            if (localStorage.getItem('timeSet') === null ) {
+                localStorage.setItem('timeSet', '');
+            }
+
+            /* folloing statements within function make timezone select input operate
+            and behave as intended */
+
+            saveSett.addEventListener('click', () => {   
+                localStorage.setItem('timeSet', `${timeZone.value}`)         
+            })
+        
+            timeZone.value = localStorage.getItem('timeSet'); 
         }
-    }
-    
-    const localStorTimeZone = function() {
 
-        /*Making the following if statement so it'll show 'select timezone'
-        when openning the site without any local storage */
-
-        if (localStorage.getItem('timeSet') === null ) {
+        cancelSett.addEventListener('click', () => {
+            emailNotif.checked = false;
+            setProf.checked = false;
+            localStorage.removeItem('emailNotifObj');
+            localStorage.removeItem('setProfObj');
+            timeZone.value = '';
             localStorage.setItem('timeSet', '');
-        }
+            })
+        
+        localStorCheckBox(emailNotif, 'emailNotifObj');
+        localStorCheckBox(setProf, 'setProfObj');
+        localStorTimeZone();
 
-        /* folloing statements within function make timezone select input operate
-        and behave as intended */
-
-        saveSett.addEventListener('click', () => {   
-            localStorage.setItem('timeSet', `${timeZone.value}`)         
-        })
-    
-        timeZone.value = localStorage.getItem('timeSet'); 
-    }
-
-cancelSett.addEventListener('click', () => {
-        localStorage.removeItem('emailNotifObj');
-        localStorage.removeItem('setProfObj');
-        localStorage.setItem('timeSet', '');
-    })
-    
-    localStorCheckBox(emailNotif, 'emailNotifObj');
-    localStorCheckBox(setProf, 'setProfObj');
-    localStorTimeZone();
-
-}    
+    }    
 
 //CALLING ALL INTERACTIVE FUNCS
 
